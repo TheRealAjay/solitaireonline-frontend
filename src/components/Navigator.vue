7a1760cbfe3f4fceee3af3deeb250e79db8a12bb
Ajay <merktmanleicht@gmail.com>
2023-11-21 13:59:10 +0100

    cleanup, routing, default views
    Signed-off-by: Ajay <merktmanleicht@gmail.com>

---
+++ src/components/Navigator.vue
@@ -0,0 +1,41 @@
+<template>
+	<nav>
+		<RouterLink to="/">Login</RouterLink>
+		<RouterLink to="/register">Registrierung</RouterLink>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: "Navigator"
+}
+</script>
+
+<style scoped>
+nav {
+	width      : 100%;
+	font-size  : 12px;
+	text-align : center;
+	margin-top : 0;
+}
+
+nav a.router-link-exact-active {
+	color : #00bd7e;
+}
+
+nav a.router-link-exact-active:hover {
+	background-color : transparent;
+}
+
+nav a {
+	display     : inline-block;
+	padding     : 0 1rem;
+	border-left : 1px solid var(--color-border);
+	color       : var(--color-text);
+}
+
+nav a:first-of-type {
+	border : 0;
+}
+
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navigator from "@/components/Navigator.vue";
+</script>
+
+<template>
+	<Navigator />
+</template>
+
+<script>
+export default {
+    name: "RegisterView"
+}
+</script>
+
+<style scoped>
+
+</style>
