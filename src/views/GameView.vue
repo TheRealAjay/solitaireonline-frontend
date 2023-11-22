ebc2e4ed27688fb2a2bb1de4ed76c84a17402e52
Ajay <merktmanleicht@gmail.com>
2023-11-22 09:49:20 +0100

    game view background
    Signed-off-by: Ajay <merktmanleicht@gmail.com>

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,10 +1,20 @@
-<template>
+<script setup>
+import background_image from "../assets/background.png";
+</script>
 
+<template>
+	<div class="gameView">
+		<div class="gameView__playArea" :style="{backgroundImage:`url(${background_image})`}">
+			<div class="gameView__playArea__drawDeck__holder">
+
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-    name: "GameView"
+	name: "GameView"
 }
 </script>
 
