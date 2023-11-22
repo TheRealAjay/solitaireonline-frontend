5c2dd88ee4247235c7a299a984b8b80e7ed4afa4
Ajay <merktmanleicht@gmail.com>
2023-11-22 12:01:08 +0100

    card art & flipping added
    Signed-off-by: Ajay <merktmanleicht@gmail.com>

---
+++ src/components/Card/PlayingCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps(['value', 'type'])
+const imageUrl = new URL(`/src/assets/cards/${props.type}_${props.value}.png`, import.meta.url).href;
+const backgroundURL = new URL(`/src/assets/cards/backart.png`, import.meta.url).href;
+</script>
+
+<template>
+	<div class="playingCard">
+		<div class="playingCard__front" :style="{backgroundImage: `url(${imageUrl})`}"></div>
+		<div class="playingCard__back" :style="{backgroundImage: `url(${backgroundURL})`}"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "PlayingCard",
+	props: {
+		value: {
+			type: Number,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,12 +1,13 @@
 <script setup>
 import background_image from "../assets/background.png";
+import PlayingCard from "@/components/Card/PlayingCard.vue";
 </script>
 
 <template>
 	<div class="gameView">
 		<div class="gameView__playArea" :style="{backgroundImage:`url(${background_image})`}">
 			<div class="gameView__playArea__drawDeck__holder">
-
+				<PlayingCard type="Clover" value="1" />
 			</div>
 		</div>
 	</div>
